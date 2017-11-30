7f699be39154584883965b7eb62e5e4ca3cb01af
ania1234 <anna.sierhej@gmail.com>
2017-11-30 21:43:39 +0100

    Rework for arrays

--- FluidSimple/Assets/FluidSolver.cs
+++ FluidSimple/Assets/FluidSolver.cs
@@ -25,13 +25,19 @@ public class FluidSolver : MonoBehaviour
 
     public void DensityStep(float diff)
     {
-        Diffuse(diff, GhostBehaviour.Copy);
-        Advect(GhostBehaviour.Copy);
+        //Diffuse(diff, GhostBehaviour.Copy);
+        //Advect(GhostBehaviour.Copy);
     }
+
+    private int Pos(int i, int j, int N)
+    {
+        return i * N + j;
+    }
+
     public void Diffuse(float diff, GhostBehaviour b)
     {
         float a = Time.deltaTime * diff * grid1.width * grid1.height;
-        LinSolve(b, a, 1 + 4 * a);
+        //LinSolve(b, a, 1 + 4 * a);
     }
 
     /// <summary>
@@ -40,48 +46,38 @@ public class FluidSolver : MonoBehaviour
     /// <param name="b">Behaviour at the boundary</param>
     /// <param name="a">Rate of aquisition from neighbours</param>
     /// <param name="c">Damping rate</param>
-    public void LinSolve(GhostBehaviour b, float a, float c)
+    public void LinSolve(GhostBehaviour b, int N, float a, float c, float[] x0, float[] x1)
     {
-
         for (int n = 0; n < iterations; n++)
         {
-            for (int i = 1; i < grid1.width - 1; i++)
+            for (int i = 1; i < N - 1; i++)
             {
-                for (int j = 1; j < grid1.height - 1; j++)
+                for (int j = 1; j < N - 1; j++)
                 {
-                    grid2.Cell(i, j).color = (grid1.Cell(i, j).color + a * (grid2.Cell(i - 1, j).color +
-                    grid2.Cell(i + 1, j).color + grid2.Cell(i, j - 1).color + grid2.Cell(i, j + 1).color)) / c;
+                    x1[Pos(i, j, N)] = x0[Pos(i, j, N)] + a * (x1[Pos(i - 1, j, N)] +
+                    x1[Pos(i + 1, j, N)] + x1[Pos(i, j - 1, N)] + x1[Pos(i, j + 1, N)]) / c;
                 }
             }
-            SetBoundary(b, grid2, (i, j) => grid2.Cell(i, j).color, (i, j, f) => grid2.Cell(i, j).color = f);
-        }
-
-
-        for (int i = 0; i < grid1.width; i++)
-        {
-            for (int j = 0; j < grid1.height; j++)
-            {
-                grid1.Cell(i, j).color = grid2.Cell(i, j).color;
-            }
+            SetBoundary(b, x1, N);
         }
     }
 
-    public void SetBoundary(GhostBehaviour b, PopulateGrid grid, Func<int, int, float> cell, Action<int, int, float> setCell)
+    public void SetBoundary(GhostBehaviour b, float[] x, int N)
     {
-        for (int i = 1; i < grid.width - 1; i++)
+        for (int i = 1; i < N - 1; i++)
         {
-            setCell(0, i, b == GhostBehaviour.MirrorHorizontal ? -1 * cell(1, i) : cell(1, i));
-            setCell(grid.width - 1, i, b == GhostBehaviour.MirrorHorizontal ? -1 * cell(grid.width - 2, i) : cell(grid.width - 2, i));
-            setCell(i, 0, b == GhostBehaviour.MirrorVertical ? -1 * cell(i, 1) : cell(i, 1));
-            setCell(i, grid.height - 1, b == GhostBehaviour.MirrorVertical ? -1 * cell(i, grid.height - 2) : cell(i, grid.height - 2));
+            x[Pos(0, i, N)] = b == GhostBehaviour.MirrorHorizontal ? -1 * x[Pos(1, i, N)] : x[Pos(1, i, N)];
+            x[Pos(N - 1, i, N)] = b == GhostBehaviour.MirrorHorizontal ? -1 * x[Pos(N - 2, i, N)] : x[Pos(N - 2, i, N)];
+            x[Pos(i, 0, N)] = b == GhostBehaviour.MirrorVertical ? -1 * x[Pos(i, 1, N)] : x[Pos(i, 1, N)];
+            x[Pos(i, N - 1, N)] = b == GhostBehaviour.MirrorVertical ? -1 * x[Pos(i, N - 2, N)] : x[Pos(i, N - 2, N)];
         }
-        setCell(0, 0, 0.5f * (cell(1, 0) + cell(0, 1)));
-        setCell(0, grid.height - 1, 0.5f * (cell(1, grid.height - 1) + cell(0, grid.height - 2)));
-        setCell(grid.width - 1, 0, 0.5f * (cell(grid.width - 2, 0) + cell(grid.width - 1, 1)));
-        setCell(grid.width - 1, grid.height - 1, 0.5f * (cell(grid.width - 2, grid.height - 1) + cell(grid.width - 1, grid.height - 2)));
+        x[Pos(0, 0, N)] = 0.5f * (x[Pos(1, 0, N)] + x[Pos(0, 1, N)]);
+        x[Pos(0, N - 1, N)] = 0.5f * (x[Pos(1, N - 1, N)] + x[Pos(0, N - 2, N)]);
+        x[Pos(N - 1, 0, N)] = 0.5f * (x[Pos(N - 2, 0, N)] + x[Pos(N - 1, 1, N)]);
+        x[Pos(N - 1, N - 1, N)] = 0.5f * (x[Pos(N - 2, N - 1, N)] + x[Pos(N - 1, N - 2, N)]);
     }
 
-    void Advect(GhostBehaviour b)
+    void Advect(GhostBehaviour b, int N, float[] c0, float[] c1, float[] vx, float[] vy)
     {
 
         float x, y, s0, t0, s1, t1, dt0;
@@ -90,30 +86,23 @@ public class FluidSolver : MonoBehaviour
         {
             for (int j = 1; j < grid1.height - 1; j++)
             {
-                x = i - dt0 * grid1.Cell(i, j).velocity.x;
-                y = j - dt0 * grid1.Cell(i, j).velocity.y;
+                x = i - dt0 * vx[Pos(i, j, N)];
+                y = j - dt0 * vy[Pos(i, j, N)];
                 if (x < 0.5f) x = 0.5f;
-                if (x > grid1.width + 0.5f) x = grid1.width + 0.5f;
+                if (x > N + 0.5f) x = N + 0.5f;
                 int i0 = (int)x;
                 int i1 = i0 + 1;
                 if (y < 0.5f) y = 0.5f;
-                if (y > grid1.height + 0.5f) y = grid1.height + 0.5f;
+                if (y > N + 0.5f) y = N + 0.5f;
                 int j0 = (int)y;
                 int j1 = j0 + 1;
                 s1 = x - i0;
                 s0 = 1 - s1;
                 t1 = y - j0;
                 t0 = 1 - t1;
-                grid2.Cell(i, j).color = s0 * (t0 * grid1.Cell(i0, j0).color + t1 * grid1.Cell(i0, j1).color) + s1 * (t0 * grid1.Cell(i1, j0).color + t1 * grid1.Cell(i1, j1).color);
+                c1[Pos(i, j, N)] = s0 * (t0 * c0[Pos(i0, j0, N)] + t1 * c0[Pos(i0, j1, N)]) + s1 * (t0 * c0[Pos(i1, j0, N)] + t1 * c0[Pos(i1, j1, N)]);
             }
         }
-        SetBoundary(b, grid2, (i, j) => grid2.Cell(i, j).color, (i, j, f) => grid2.Cell(i, j).color = f);
-        for (int i = 0; i < grid1.width; i++)
-        {
-            for (int j = 0; j < grid1.height; j++)
-            {
-                grid1.Cell(i, j).color = grid2.Cell(i, j).color;
-            }
-        }
+        SetBoundary(b, c1, N);
     }
 }
